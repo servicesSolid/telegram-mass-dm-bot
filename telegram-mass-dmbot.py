332e16c4d6d3f60738a9e065cecac1ce5641a0d2
Solid Services <nicobrust750@gmail.com>
2026-06-30 00:06:15 +0300

    Add files via upload

---
+++ telegram-mass-dmbot.py
@@ -0,0 +1,6 @@
+print("Please read README.MD file for details.")
+# Check readme file and contact us via Telegram / Website 
+# Check readme file and contact us via Telegram / Website 
+# Check readme file and contact us via Telegram / Website 
+# Check readme file and contact us via Telegram / Website 
+# Check readme file and contact us via Telegram / Website 
